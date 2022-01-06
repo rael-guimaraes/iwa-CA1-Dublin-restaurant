5d1e485c2554ed3ea0cf3487615f6b219d15ac90
rael-guimaraes <raelguimaraes8@hotmail.com>
2022-01-06 21:10:30 +0000

    adding comments to the index.js file

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ const   express = require('express'), //Allows to respond to HTTP requests, defi
 const   router = express(), //Instantiating Express
         server = http.createServer(router); //Instantiating the server
 
-router.use(express.static(path.resolve(__dirname,'view'))); //Serving static content from "views" folder
+router.use(express.static(path.resolve(__dirname,'view'))); //Serving static content from "view" folder
 router.use(express.json());
 
+// converts from XML to JSON
 function XMLtoJSON(filename, cb){
     let filepath = path.normalize(path.join(__dirname, filename));
     fs.readFile(filepath, 'utf8', function(err, xmlStr){
@@ -20,6 +21,7 @@ function XMLtoJSON(filename, cb){
     });
 };
 
+// converts from JSON to XML
 function JSONtoXML(filename, obj, cb) {
     let filepath = path.normalize(path.join(__dirname, filename));
     let builder = new xml2js.Builder();
@@ -35,35 +37,31 @@ router.get('/get/html', function(req, res) {
     let xml = fs.readFileSync('DublinRestaurant.xml', 'utf8'), //read in the XML file
         xsl = fs.readFileSync('DublinRestaurant.xsl', 'utf8'); //read in the XSL file
 
-    // console.log(xml);
-    // console.log(xsl);
-
     let doc = xmlParse(xml), //Parse the XML file
         stylesheet = xmlParse(xsl); //Parse the XSL file
 
     let result = xsltProcess(doc, stylesheet); //Performing XSLT
 
-    // console.log(result);
 
     res.end(result.toString()); //Serve back the user
 
 });
 
 router.post('/post/json', function(req, res) {
-
+// 
     console.log(req.body);
-
+    // function to add new entry
     function appendJSON(obj) {
 
         console.log(JSON.stringify(obj, null, " "))
-
+        // receives the entry as XML and converts it to JSON
         XMLtoJSON('DublinRestaurant.xml', function (err, result){
             if (err) throw (err);
-
+                // recieve the parameters of the new entry and adds to the menu
             result.restaurant_menu.week_day[obj.w_day].meal.push({'item': obj.item, 'price': obj.price});
 
             console.log(JSON.stringify(result, null, " "));
-
+            // once the new entry is added converts back to XML
             JSONtoXML('DublinRestaurant.xml', result, function(err){
                 if (err) console.log(err);
             });
@@ -81,20 +79,20 @@ router.post('/post/json', function(req, res) {
 router.post('/post/delete', function(req, res){
 
     console.log(req.body);
-
+// function to delete selected entry
     function deleteJSON(obj){
 
         console.log(obj)
-
+        // receives the selected entry to delete as XML and converts it to JSON
         XMLtoJSON('DublinRestaurant.xml', function(err, result){
             if (err) throw (err);
 
             console.log(obj.sec);
             console.log(obj.ent);
             console.log(result);
-            
+            // recieve the parameters of the selected entry to delete and removes it from the menu
             delete result.restaurant_menu.week_day[obj.sec].meal[obj.ent];
-
+            // once the selected entry is removed, it converts back to XML
             JSONtoXML('DublinRestaurant.xml', result, function(err){
                 if (err) console.log(err);
             });
@@ -106,7 +104,7 @@ router.post('/post/delete', function(req, res){
     res.redirect('back');
 
 });
-
+// define which port will be used and sets the default ip address
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
     const addr = server.address();
     console.log('Server listening at', addr.address + ':' + addr.port)
